708360e696bd2b6f9a179eaba78ad4fd001f9dd5
yma6375323 <40642798+yma6375323@users.noreply.github.com>
2019-02-12 02:32:24 -0800

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,6 @@
+sample01: sample01.cpp
+	g++ sample01.cpp -o sample01
+myProg01: myProg01.cpp
+	g++ myProg01.cpp -o myProg0
+countDucks: countDucks.cpp
+	g++ countDucks.cpp -o countDucks
---
+++ myProg01.cpp
@@ -0,0 +1,17 @@
+// myProg.cpp
+//Author: Doris Ma
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    // Simple program that counts down from n to 1
+ 
+    int n=5;
+ 
+    for (int i=n; i>0; i--) {
+       cout <<i<<" "; 
+
+    }
+ 	cout<<"\n";
+    return 0;
+}
